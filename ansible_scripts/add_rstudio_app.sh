16d0cb4f645f4ede6fbec4a487c0f1179115fe06
Audrey Stott <audrey.stott@pawsey.org.au>
2021-05-26 13:38:07 +0000

    edited ansible-rstudio scripts

--- ansible_scripts/add_rstudio_app.sh
+++ ansible_scripts/add_rstudio_app.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-sudo microk8s kubectl apply -f rstudio-deployment.yaml --env="PASSWORD={{ rstudio_password }}"
-sudo microk8s kubectl expose deployment rstudio-deployment --target-port=8787 --name=rstudio-server --type=NodePort
-
+pubkey_path="$1"
+rstudio_password="$2"
+rstudio_port_number=$(sudo microk8s kubectl get svc | grep -oP '8787:.{0,5}' | sed 's/^.*://')
+public_ip_address=$(curl ifconfig.me)
 
 echo '------------------------------------------------------------------------------------------- 
 
 
 Run the following command on your local computer to enable port forwarding from this instance to your local computer:
-ssh -i {{ pubkey_path }} -N -f -L {{ rstudio_port_number }}:localhost:{{ rstudio_port_number }} ubuntu@{{ public_ip_address }}
+ssh -i '$pubkey_path' -N -f -L '$rstudio_port_number':localhost:'$rstudio_port_number' ubuntu@'$public_ip_address'
 
 Then go to a web browser and enter the following URL to run RStudio:
-http://localhost:{{ rstudio_port_number }}
+http://localhost:'$rstudio_port_number'
 
 ****After exiting RStudio, close the 8787 port on your local computer****:
 lsof -ti:8787 | xargs kill -9
