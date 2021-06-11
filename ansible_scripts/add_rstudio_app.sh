d91707f93b1b3f30b31ca022f64a24c1b244ae4f
Audrey Stott <audrey.stott@pawsey.org.au>
2021-06-11 12:51:38 +0800

    edited scripts

--- ansible_scripts/add_rstudio_app.sh
+++ ansible_scripts/add_rstudio_app.sh
@@ -10,7 +10,7 @@ echo '--------------------------------------------------------------------------
 
 Run the following command on your local computer to enable port forwarding from this instance to your local computer: ssh -i '$pubkey_path' -N -f -L '$rstudio_port_number':localhost:'$rstudio_port_number' ubuntu@'$public_ip_address'
 
-Then go to a web browser and enter the following URL to run RStudio: http://localhost:'$rstudio_port_number'
+Then go to a web browser and enter the following URL to run RStudio: http://localhost:'$rstudio_port_number'. The username is rstudio and the password is what your entered when prompted at the start.
 
 ****After exiting RStudio, close the '$rstudio_port_number' port on your local computer****: lsof -ti:'$rstudio_port_number' | xargs kill -9
 
