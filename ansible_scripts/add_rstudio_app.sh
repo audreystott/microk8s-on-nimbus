38cbf9ec8db014bb842f17e1bada382eb5d2d143
audreystott <audrey.stott@pawsey.org.au>
2021-08-18 16:28:21 +0000

    images on registry

--- ansible_scripts/add_rstudio_app.sh
+++ ansible_scripts/add_rstudio_app.sh
@@ -5,13 +5,21 @@ rstudio_password="$2"
 rstudio_port_number=$(sudo microk8s kubectl get svc | grep -oP '8787:.{0,5}' | sed 's/^.*://')
 public_ip_address=$(curl ifconfig.me)
 
-echo '------------------------------------------------------------------------------------------- 
+echo "-----------------------------------------------------------------------------------------------------------------------------------
 
 
-Run the following command on your local computer to enable port forwarding from this instance to your local computer: ssh -i '$pubkey_path' -N -f -L '$rstudio_port_number':localhost:'$rstudio_port_number' ubuntu@'$public_ip_address'
+        Run the following command on your local computer to enable port forwarding from this instance to your local computer:       
+                ssh -i $pubkey_path -N -f -L {{ rstudio_port_number }}:localhost:{{ rstudio_port_number }} ubuntu@$public_ip_address                                         
 
-Then go to a web browser and enter the following URL to run RStudio: http://localhost:'$rstudio_port_number'. The username is rstudio and the password is what your entered when prompted at the start.
+        Then go to a web browser and enter the following URL to run RStudio:                                                        
+                http://localhost:{{ rstudio_port_number }}                                                                                              
+                username = rstudio                                                                                                   
+                password = what your entered when prompted at the start                                                             
 
-****After exiting RStudio, close the '$rstudio_port_number' port on your local computer****: lsof -ti:'$rstudio_port_number' | xargs kill -9
+        All RStudio sessions and associated data will be saved under /data/rstudio.                                                                    
 
--------------------------------------------------------------------------------------------'
+        IMPORTANT: After exiting RStudio, close the 8787 port on your local computer:                                               
+                   lsof -ti:8787 | xargs kill -9                                                                                    
+
+
+-----------------------------------------------------------------------------------------------------------------------------------"
